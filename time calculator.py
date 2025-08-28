dbe072c358a5906d4e929f0e49a3d8c764b38511
Brian Juarez <139927665+Brian-ai-cmd@users.noreply.github.com>
2025-08-27 18:31:56 -0600

    Adding second function

--- time calculator.py
+++ time calculator.py
@@ -6,8 +6,15 @@ def turn_minutes_into_hours(minutes):
     else:
         return 0, minutes
     
-
-
+def turn_hours_into_days(hours):
+    if hours >= 24:
+        days = hours // 24
+        hours = hours % 24
+        return days, hours
+    else:
+        return 0, hours
+    
+    
 
 
 
